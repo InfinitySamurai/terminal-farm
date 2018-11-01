9f0c182831a90a44f70d2a118c1596e55c0163f5
Russell Long <russelllongj@gmail.com>
2018-11-01 20:28:08 +1100

    Added typing support and checks

--- game.py
+++ game.py
@@ -4,3 +4,16 @@ p: Plant = Plant()
 print(p)
 p.water()
 print(p)
+
+
+def testFunction(plant: Plant):
+    print("We got passed a plant")
+    print(plant)
+
+
+def wantsAString(thing: str):
+    print(thing)
+
+
+wantsAString(1)
+testFunction("not a plant")
