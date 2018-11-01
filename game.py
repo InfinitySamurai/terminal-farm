ec550413846e63ced0d0f4c8401651d31462c0c0
Russell Long <russelllongj@gmail.com>
2018-11-01 19:55:00 +1100

    Removed placeholder code

--- game.py
+++ game.py
@@ -1,16 +1,6 @@
 from Plant import Plant
 
-print('Hello World')
-
-
-def badThing(alpha, beta):
-    print("Are there any non flakey things here?")
-    print(alpha+beta)
-
-
-badThing(40, 123)
-
-p = Plant()
+p: Plant = Plant()
 print(p)
 p.water()
 print(p)
