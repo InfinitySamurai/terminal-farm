d2a9b66076eee8beff14e4acfc5843479d8d8580
Russell Long <russelllongj@gmail.com>
2018-11-08 21:05:15 +1100

    Updated plots to reference adjacent plots. New PlotContainer contains plots

--- Plot.py
+++ Plot.py
@@ -1,8 +1,21 @@
-from Plant import Plant
+from typing import Any
 
 
 class Plot():
     def __init__(self):
-        self.plant: Plant = None
+        self.object: Any = None
         self.tilled: bool = False
         self.color: str = "#ffffff"
+        self.watered: bool = False
+        self.upPlot: Plot = None
+        self.rightPlot: Plot = None
+        self.downPlot: Plot = None
+        self.leftPlot: Plot = None
+
+    def __str__(self):
+        plotState = """object: {}
+        tilled: {}
+        color: {}
+        watered: {}
+        """.format(self.object, self.tilled, self.color, self.watered)
+        return plotState
