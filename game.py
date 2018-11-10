3cff173c858a9a7e1861a5fc3e4425f9e1980cf3
Russell Long <russelllongj@gmail.com>
2018-11-10 21:51:49 +1100

    Changed game to simply render the plotcontainer

--- game.py
+++ game.py
@@ -1,37 +1,40 @@
 from Plant import Plant
 from Menu import Menu
+from PlotContainer import PlotContainer
 from bearlibterminal import terminal
 
-gameObjects = []
-p: Plant = Plant("Apple Tree", 10, "@")
-gameObjects.append(p)
+# gameObjects = []
+# p: Plant = Plant("Apple Tree", 10, "@")
+# gameObjects.append(p)
 
 
-def updateAllGameObjects():
-    for object in gameObjects:
-        object.update()
-        object.display(terminal, 30, 30)
+# def updateAllGameObjects():
+#     for object in gameObjects:
+#         object.update()
+#         object.display(terminal, 30, 30)
 
 
 terminal.open()
+# menu: Menu = Menu(terminal, 30)
+# menu.addOption("Water Plant", p.water)
+# menu.addOption("Advance Time", updateAllGameObjects)
+# menu.displayOptions(10, 10)
 
-menu: Menu = Menu(terminal, 30)
-menu.addOption("Water Plant", p.water)
-menu.addOption("Advance Time", updateAllGameObjects)
-menu.displayOptions(10, 10)
 
-
-p.display(terminal, 30, 30)
+# p.display(terminal, 30, 30)
 # p.update()
 # p.display(terminal, 31, 30)
 
-while terminal.peek() != terminal.TK_CLOSE:
-    input = terminal.read()
-    if(input == 30):
-        menu.selectOption(0)
-    elif(input == 31):
-        menu.selectOption(1)
-    elif(input == 224 or input == 41):
-        terminal.close()
+pc = PlotContainer(5, 5, 10, 5)
+pc.display(terminal)
 
-terminal.close()
+while terminal.peek() != terminal.TK_CLOSE:
+    userInput = terminal.read()
+    if(userInput == 30):
+        # menu.selectOption(0)
+        print("nothing")
+    elif(userInput == 31):
+        # menu.selectOption(1)
+        print("nothing2")
+    elif(userInput == 224 or userInput == 41):
+        terminal.close()
