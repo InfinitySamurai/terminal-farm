b6ef274e4f3bc5f601a19cd4157b1810ce3dd462
Russell Long <russelllongj@gmail.com>
2018-11-10 21:51:22 +1100

    Added display methods for plots, renamed some variables

--- PlotContainer.py
+++ PlotContainer.py
@@ -4,20 +4,22 @@ from typing import List, Any, Union
 
 class PlotContainer():
 
-    def __init__(self, x, y, maxX, maxY):
+    def __init__(self, x, y, width, height):
         self.x: int = x
         self.y: int = y
-        self.maxX: int = maxX
-        self.maxY: int = maxY
-        self.topLeftPlot: Plot = self.createPlots(maxX, maxY)
 
-    def createPlots(self, xSize, ySize) -> Plot:
+        self.width: int = width
+        self.height: int = height
+
+        self.topLeftPlot: Plot = self.createPlots(self.width, self.height)
+
+    def createPlots(self, width, height) -> Plot:
         firstPlot: Union[Plot, None] = None
         previousPlotRow: List[Plot] = []
-        for i in range(xSize):
+        for i in range(height):
             previousPlot: Union[Plot, None] = None
             currentPlotRow: List[Plot] = []
-            for j in range(ySize):
+            for j in range(width):
                 newPlot: Plot = Plot()
                 if(i == 0 and firstPlot is None):
                     firstPlot = newPlot
@@ -32,8 +34,22 @@ class PlotContainer():
             previousPlotRow = currentPlotRow
         return firstPlot
 
-    def addObject(self, o: Any, x: int, y: int):
-        if (x > self.maxX):
-            raise Exception("PlotContainer x: {} too large for maxX: {}".format(x, self.maxX))
-        if (y > self.maxY):
-            raise Exception("PlotContainer y: {} too large for maxY: {}".format(y, self.maxY))
+    # def addObject(self, o: Any, x: int, y: int):
+    #     if (x > self.maxX):
+    #         raise Exception("PlotContainer x: {} too large for maxX: {}".format(x, self.maxX))
+    #     if (y > self.maxY):
+    #         raise Exception("PlotContainer y: {} too large for maxY: {}".format(y, self.maxY))
+
+    def display(self, terminal):
+        self.displayRow(terminal, self.topLeftPlot, self.x, self.y)
+        terminal.refresh()
+
+    def displayRow(self, terminal, firstPlot, x, y):
+        self.displayPlot(terminal, firstPlot, x, y)
+        if(firstPlot.downPlot):
+            self.displayRow(terminal, firstPlot.downPlot, x, y + 1)
+
+    def displayPlot(self, terminal, plot, x, y):
+        plot.display(terminal, x, y)
+        if(plot.rightPlot):
+            self.displayPlot(terminal, plot.rightPlot, x + 1, y)
